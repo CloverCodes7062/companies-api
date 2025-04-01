e1ad8e370d270aca8231693f78e63cf91f018064
Stacy McCarrell <133838749+CloverCodes7062@users.noreply.github.com>
2025-03-31 22:30:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup as bs
 
 app = FastAPI()
 
+@app.get("/companies")
+def get_companies():
+    r = requests.get("https://stockanalysis.com/list/sp-500-stocks/")
+    soup = bs(r.content, "html.parser")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    companies = []
+    for tr in soup.find_all("tr"):
+        tds = tr.find_all("td")
+        if tds and len(tds) >= 7:
+            companies.append({
+                "No.": tds[0].text,
+                "Symbol": tds[1].text,
+                "Company Name": tds[2].text,
+                "Market Cap": tds[3].text,
+                "Stock Price": tds[4].text,
+                "% Change": tds[5].text,
+                "Revenue": tds[6].text
+            })
+    
+    return {"companies": companies}
